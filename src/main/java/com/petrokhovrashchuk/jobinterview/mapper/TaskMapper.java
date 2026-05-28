6854d7c0c28e48ac7255dd2c08fee03dac246dfc
Petro Khovrashchuk <petro.khovrashchuk@gmail.com>
2026-05-28 15:28:18 +0200

    abstract TaskMapper into interface

--- src/main/java/com/petrokhovrashchuk/jobinterview/mapper/TaskMapper.java
+++ src/main/java/com/petrokhovrashchuk/jobinterview/mapper/TaskMapper.java
@@ -3,22 +3,13 @@ package com.petrokhovrashchuk.jobinterview.mapper;
 import com.petrokhovrashchuk.jobinterview.dto.TaskDto;
 import com.petrokhovrashchuk.jobinterview.model.Task;
 import java.util.Collection;
-import org.springframework.stereotype.Component;
 
-@Component
-public class TaskMapper {
+public interface TaskMapper {
 
-  public TaskDto modelToDto(final Task task) {
-    return new TaskDto(task.getId(), task.getTitle(), task.getDescription(), task.isCompleted());
-  }
+  TaskDto modelToDto(final Task task);
 
-  public Collection<TaskDto> modelsToDtos(final Collection<Task> tasks) {
-    return tasks.stream().map(this::modelToDto).toList();
-  }
+  Collection<TaskDto> modelsToDtos(final Collection<Task> tasks);
 
-  public Task dtoToModel(final TaskDto taskDto) {
-    return new Task(taskDto.id(), taskDto.title(),
-        taskDto.description(), taskDto.completed());
-  }
+  Task dtoToModel(final TaskDto taskDto);
 
 }
